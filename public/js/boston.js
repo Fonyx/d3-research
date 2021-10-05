2ad564372763e733b5278d7be9d7448343008305
Nick <nick.alex.ritchie@gmail.com>
2021-10-05 12:48:52 +1100

    scaled dataset based on min max

--- public/js/boston.js
+++ public/js/boston.js
@@ -1,4 +1,8 @@
-var width = 1000, height = 500;
+var width = 1000, height = 500, margin = 150;
+
+var marginWidth = width-margin;
+var marginHeight = height-margin;
+
 var circles;
 
 var svg = d3.select('#chart')
@@ -7,27 +11,34 @@ var svg = d3.select('#chart')
     .attr('height', height + 'px');
 
 
-// async function loadBoston(){
-//     let response = await fetch('http://localhost:3000/api/boston');
-//     let data = await response.json();
-//     return data
-// }
-
-// loadBoston();
-
-
 var data = d3.json('http://localhost:3000/api/boston')
     .then((data)=>{
+
+        var xMinMax = d3.extent(data, (element)=>{
+            return parseFloat(element.poor);
+        });
+        
+        xScale = d3.scaleLinear()
+            .domain([xMinMax[0], xMinMax[1]])
+            .range([margin, marginWidth]);
+                var yMinMax = d3.extent(data, (element)=>{
+            return parseFloat(element.rooms);
+        });
+        
+        yScale = d3.scaleLinear()
+            .domain([yMinMax[0], yMinMax[1]])
+            .range([margin, marginHeight]);
+        
         circles = svg.selectAll('.dot')
         .data(data)
         .enter()
         .append('circle')
         .attr('class', 'dot')
         .attr('cx',function(d){
-            return parseFloat(d.poor);
+            return xScale(d.poor);
         })
         .attr('cy',function(d){
-            return parseFloat(d.rooms) * 50;
+            return yScale(d.rooms);
         })
         .attr('r',function(d){
             return parseFloat(d.value)/2;
