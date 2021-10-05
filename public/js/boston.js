631747739161d0a0463de795ae93ebf98046d33b
Nick <nick.alex.ritchie@gmail.com>
2021-10-05 14:04:54 +1100

    labels

--- public/js/boston.js
+++ public/js/boston.js
@@ -4,6 +4,9 @@ var marginWidth = width-margin;
 var marginHeight = height-margin;
 var minDotSize = 2, maxDotSize = 15;
 
+var xMinMax, yMinMax, rMinMax;
+var xScale, yScale, rScale;
+
 var svg = d3.select('#chart')
     .append('svg')
     .attr('width', width + 'px')
@@ -22,15 +25,13 @@ function getExtentRange(data, label){
  * @param {[list{object}]} data 
  * @param {'String'} label 
  */
-function getHorizontalScale(data, label){
+function getHorizontalScale(data, label, minMax){
 
-    let minMax = getExtentRange(data, label);
-    
-    Scale = d3.scaleLinear()
-        .domain([minMax[1], minMax[0]])
+    let scale = d3.scaleLinear()
+        .domain([minMax[0], minMax[1]])
         .range([margin, marginWidth]);
 
-    return Scale;
+    return scale;
 }
 
 /**
@@ -38,14 +39,13 @@ function getHorizontalScale(data, label){
  * @param {[list{object}]} data 
  * @param {'String'} label 
  */
-function getVerticalScale(data, label){
+function getVerticalScale(data, label, minMax){
 
-    let minMax = getExtentRange(data, label);
-    Scale = d3.scaleLinear()
+    let scale = d3.scaleLinear()
         .domain([minMax[1], minMax[0]])
         .range([margin, marginHeight]);
 
-    return Scale;
+    return scale;
 }
 
 /**
@@ -53,42 +53,30 @@ function getVerticalScale(data, label){
  * @param {[list{object}]} data 
  * @param {'String'} label 
  */
-function getSizeScale(data, label){
+function getSizeScale(data, label, minMax){
 
-    let minMax = getExtentRange(data, label);
-
-    Scale = d3.scaleSqrt()
+    let scale = d3.scaleSqrt()
         .domain([minMax[0], minMax[1]])
         .range([minDotSize, maxDotSize]);
 
-    return Scale;
-}
-
-/**
- * collect a square root scale in the size dimension for the extent of the data given a specific label
- * @param {[list{object}]} data 
- * @param {'String'} label 
- */
-function getColorScale(data, label){
-
-    let minMax = getExtentRange(data, label);
-
-    Scale = d3.scaleOrdinal()
-        .domain([minMax[0], minMax[1]])
-        .range(['#FA7D97', '#64D0FA']);
-        //'#AD455A', '#FAF596', '#FA7D97', '#64D0FA', '#4E93AD']);
-
-    return Scale;
+    return scale;
 }
 
 
 var data = d3.json('http://localhost:3000/api/boston')
     .then((data)=>{
 
-        let xScale = getHorizontalScale(data, 'poor');
-        let yScale = getVerticalScale(data, 'rooms');
-        let rScale = getSizeScale(data, 'value');
-        let cScale = getColorScale(data, 'charles')
+        xMinMax = getExtentRange(data, 'poor');
+        yMinMax = getExtentRange(data, 'rooms');
+        rMinMax = getExtentRange(data, 'value');
+        xScale = getHorizontalScale(data, 'poor', xMinMax);
+        yScale = getVerticalScale(data, 'rooms', yMinMax);
+        rScale = getSizeScale(data, 'value', rMinMax);
+
+        // sort by charles value
+        data = data.sort(function(a, b){
+            return a.charles - b.charles;
+        });
             
         var dots = svg.selectAll('.dot')
             .data(data)
@@ -105,8 +93,24 @@ var data = d3.json('http://localhost:3000/api/boston')
             })
             // change class assigned based on the charles boolean for fill rendering
             .attr('class', (d)=>{
-                return parseInt(d.charles) ? 'charles-dot': 'non-charles-dot';
+                return parseInt(d.charles) ? 'dot charles': 'dot non-charles';
             })
+        
+        // axis details
+        xAxis = d3.axisBottom(xScale).tickValues([xMinMax[0], xMinMax[1]]);
+        xAxisG = svg.append('g')
+            .attr('id', 'xAxis')
+            .attr('class', 'axis');
 
+        xAxisG.call(xAxis)
+            .attr('transform', 'translate(0,'+marginHeight+')');
+            
+        yAxis = d3.axisLeft(yScale).tickValues([yMinMax[0], yMinMax[1]]);
+        yAxisG = svg.append('g')
+        .attr('id', 'yAxis')
+        .attr('class', 'axis');
+
+        yAxisG.call(yAxis)
+            .attr('transform', 'translate('+marginWidth+', 0)');
     })
 
